ee8eb2bc03031d2880800e50f2ea49a73ac5ba36
Patrick Duvall <duvallp208@gmail.com>
2019-05-06 16:20:57 -0600

    Add author model test
    Co-authored-by: Vince Carollo <vincecarollo@gmail.com>

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe Book do
+  describe "validations" do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:published)}
+    it {should validate_presence_of(:pages)}
+    it {should have_many(:authors)}
+    it {should belong_to(:authors)}
+  end
+
+
+end
