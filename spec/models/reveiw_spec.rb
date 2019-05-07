c6d3cdacbe3e007ae9fd4bff550cb12c0db0f140
Vince Carollo <vincecarollo@gmail.com>
2019-05-07 11:48:32 -0600

    Change model specs to check for relationships

--- spec/models/reveiw_spec.rb
+++ spec/models/reveiw_spec.rb
@@ -1,13 +1,16 @@
 require "rails_helper"
 
 describe Review do
+
   describe "validations" do
     it {should validate_presence_of(:title)}
     it {should validate_presence_of(:body)}
     it {should validate_presence_of(:rating)}
+  end
+
+  describe "relationships" do
     it {should belong_to(:book)}
     it {should belong_to(:user)}
   end
-
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
 
 describe User do
+
   describe "validations" do
     it {should validate_presence_of(:name)}
-    it {should have_many(:reviews)}
   end
 
+  describe "relationships" do
+    it {should have_many(:reviews)}
+  end
 
 end
