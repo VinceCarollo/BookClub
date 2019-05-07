c2be186d03f332354064367c2eb8c984daee6d00
Vince Carollo <vincecarollo@gmail.com>
2019-05-07 11:45:52 -0600

    Add migrations for reviews and users tables
    Co-authored-by: Patrick Duvall <duvallp208@gmail.com>

---
+++ db/migrate/20190507173729_create_review.rb
@@ -0,0 +1,13 @@
+class CreateReview < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.string :title
+      t.integer :rating
+      t.string :body
+      t.references :user, foreign_key: true
+      t.references :book, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20190507174059_add_review_to_user.rb
@@ -0,0 +1,5 @@
+class AddReviewToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :users, :review, foreign_key: true
+  end
+end
