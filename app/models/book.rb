82962e8042a65c7d857346b112d58cdeaf93992f
Vince Carollo <vincecarollo@gmail.com>
2019-05-07 18:28:58 -0600

    Starts rating sort ascending method in book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,17 +3,19 @@ class Book < ApplicationRecord
   has_many :reviews
   has_many :authors, through: :book_authors
 
-
   validates_presence_of :title, :pages, :published
 
+  def self.rating_sort_asc
+    binding.pry
+  end
 
   def review_count
     reviews.count
   end
+
   def review_average
     self.reviews.average(:rating)
     # require "pry"; binding.pry
   end
-
 
 end
