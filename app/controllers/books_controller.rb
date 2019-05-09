a0dbde6eabad37de945bc2c297fa3c205aaf8a22
Vince Carollo <vincecarollo@gmail.com>
2019-05-09 15:34:23 -0600

    Add needed ivars to books controller in show action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,11 +22,13 @@ class BooksController < ApplicationController
     @highest_3 =Book.highest_3_rated_titles
     @lowest_3 = Book.lowest_3_rated_titles
     @three_ratingest_users = User.three_ratingest_users
-
   end
 
   def show
     @book = Book.find(params[:id])
+    @top_three_reviews = @book.reviews.top_three
+    @bottom_three_reviews = @book.reviews.bottom_three
+    @average_rating = @book.reviews.average_rating
   end
 
 end
