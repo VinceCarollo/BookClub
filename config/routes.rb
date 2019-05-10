911df1be2f6b804847d1aa223ea959b602cc8e66
Vince Carollo <vincecarollo@gmail.com>
2019-05-10 13:09:36 -0600

    Nest new book review route in books route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show] do
+    resources :reviews, only: [:new]
+  end
+
   resources :authors, only: :show
 end
