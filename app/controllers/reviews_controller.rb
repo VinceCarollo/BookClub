39050c92572067289cd6447431ab849181382bda
Vince Carollo <vincecarollo@gmail.com>
2019-05-11 16:13:24 -0600

    Add validation of rating in create action of reviews controller before creating review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,14 +7,22 @@ class ReviewsController < ApplicationController
 
   def create
     new_review = Review.new(review_params)
-    new_review.user = User.find_or_create_by(name: params[:review][:username])
-    new_review.book = Book.find(params[:book_id])
-    new_review.save
-
-    redirect_to book_path(params[:book_id])
+    book = Book.find(params[:book_id])
+    validation(new_review, book)
   end
 
   private
+
+  def validation(review, book)
+    if review.rating > 5 || review.rating <= 0
+      redirect_to new_book_review_path(book)
+    else
+      review.user = User.find_or_create_by(name: params[:review][:username])
+      review.book = book
+      review.save
+      redirect_to book_path(book)
+    end
+  end
 
   def review_params
     params.require(:review).permit(:title, :rating, :body)
