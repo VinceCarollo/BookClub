87c94190e79bf763c28d4030e8c669d12cacb079
Vince Carollo <vincecarollo@gmail.com>
2019-05-10 22:26:22 -0600

    Add case statement to check for sorting queries users controller to index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @user_reviews = Review.find_all_user_reviews(@user)
+
+    case params[:sort]
+    when 'newest'
+      @user_reviews = Review.find_user_reviews(@user).sort_by(&:created_at).reverse
+    when 'oldest'
+      @user_reviews = Review.find_user_reviews(@user).sort_by(&:created_at)
+    else
+      @user_reviews = Review.find_user_reviews(@user)
+    end
   end
 
 end
