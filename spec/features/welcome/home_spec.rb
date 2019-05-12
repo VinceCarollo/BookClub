f4fceab7db2d002a2bce6f810eda1c720eb82fee
Patrick Duvall <duvallp208@gmail.com>
2019-05-12 14:17:30 -0600

    Add nav partial and render nav in welcome

--- spec/features/welcome/home_spec.rb
+++ spec/features/welcome/home_spec.rb
@@ -4,9 +4,16 @@ describe "as a visitor" do
   describe "when I visit the home page" do
     it "says welcome" do
       visit '/'
+      save_and_open_page
       expect(page).to have_content("Welcome")
     end
 
-
+    it "can redirect to books index and back" do
+        visit '/'
+      click_link "Books"
+      expect(current_path).to eq(books_path)
+      click_link "Home"
+      expect(current_path).to eq('/')
+    end
   end
 end
