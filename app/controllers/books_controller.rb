6bf0d605ce140704e38bfc4904ff0e66c92b9b64
Patrick Duvall <duvallp208@gmail.com>
2019-05-14 11:33:03 -0600

    Minorly refactor index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,20 +3,13 @@ class BooksController < ApplicationController
   def index
     User.three_ratingest_users
     case params[:sort]
-    when 'pagesasc'
-      @books = Book.page_sort_asc
-    when 'pagesdesc'
-        @books = Book.page_sort_desc
-    when 'publishedasc'
-        @books = Book.published_sort_asc
-    when 'publisheddesc'
-      @books = Book.published_sort_desc
-    when 'reviewsasc'
-      @books = Book.reviews_sort_asc
-    when 'reviewsdesc'
-      @books = Book.reviews_sort_desc
-    else
-      @books = Book.all
+    when 'pagesasc'     ; @books = Book.page_sort_asc
+    when 'pagesdesc'    ; @books = Book.page_sort_desc
+    when 'publishedasc' ; @books = Book.published_sort_asc
+    when 'publisheddesc'; @books = Book.published_sort_desc
+    when 'reviewsasc'   ; @books = Book.reviews_sort_asc
+    when 'reviewsdesc'  ; @books = Book.reviews_sort_desc
+    else ; @books = Book.all
     end
 
     @highest_3 =Book.highest_3_rated_titles
