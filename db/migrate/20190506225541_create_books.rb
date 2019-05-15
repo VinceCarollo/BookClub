f6ef3fe200888faef033410003275708fc6b5d28
Patrick Duvall <duvallp208@gmail.com>
2019-05-14 18:28:04 -0600

    Remove book default image in schema

--- db/migrate/20190506225541_create_books.rb
+++ db/migrate/20190506225541_create_books.rb
@@ -4,7 +4,7 @@ class CreateBooks < ActiveRecord::Migration[5.1]
       t.string :title
       t.integer :published
       t.integer :pages
-      t.string :image_url, default: 'https://timedotcom.files.wordpress.com/2015/06/521811839-copy.jpg'
+      t.string :image_url
     end
   end
 end
