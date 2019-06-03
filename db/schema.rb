5045d9467d9794f95475b2984f8453d9d0e786c1
Vince Carollo <vincecarollo@gmail.com>
2019-06-02 23:07:12 -0600

    Add role attribute to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190507173729) do
+ActiveRecord::Schema.define(version: 20190603050440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20190507173729) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0
   end
 
   add_foreign_key "book_authors", "authors"
